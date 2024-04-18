0db2d3f4f6b458ec29b89c4b52848e2687102f21
gaverver <gavrielsh06@gmail.com>
2024-04-18 12:37:27 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.test:monitor:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
